3393f12c94bee3225c68f6a81a93db5b2eb6544c
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2016-01-26 13:01:07 +0200

    Fixed Selenium unit test

--- tests/Example/SeleniumExampleTest.php
+++ tests/Example/SeleniumExampleTest.php
@@ -2,19 +2,19 @@
 namespace Tests\Example;
 /**
  * Selenium Example Test
- * 
+ *
  * In order to be able to run this test, you'll need to:
- * 
+ *
  * 1. Download the Selenium Server JAR file
  * (selenium-server-standalone-2.xx.y.jar) from
  * http://docs.seleniumhq.org/download/
- * 
+ *
  * 2. Start the Selenium Server with: java -jar selenium-server-...jar
- * 
+ *
  * 3. Start the PHP webserver with: php -S localhost:8000
- * 
- * 4. Run the phpunit 
- * 
+ *
+ * 4. Run the phpunit
+ *
  * @author Antonis Flangofas <a.flangofas@qobo.biz>
  * @group example
  * @group selenium
@@ -32,8 +32,8 @@ class SeleniumExampleTest extends \PHPUnit_Extensions_Selenium2TestCase
 
 	public function testTitle()
 	{
-		$this->url('index.php');
-		$this->assertContains('phpinfo', $this->title());
+		$this->url('/');
+		$this->assertContains('Pages', $this->title());
 	}
 
 }
