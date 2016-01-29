eaeac84dd468a84370f7788758c82d0ce208c109
George Constantinou <g.constantinou@qobo.biz>
2016-01-29 15:41:35 +0200

    added migrations task, useful for handling plugin migrations commands, currently handling migrate command (task #796)

---
+++ src/Shell/Task/MigrationsTask.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         3.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Shell\Task;
+
+use App\Shell\Task\ListTask;
+use Cake\Console\Shell;
+use Cake\Core\Plugin;
+
+/**
+ * Task for migrating plugins.
+ * @todo add support for CakePHP Migrations rollback, status and mark_migrated commands
+ *
+ */
+class MigrationsTask extends Shell
+{
+    /**
+     * Tasks to load
+     *
+     * @var array
+     */
+    public $tasks = [
+        'Migrate'
+    ];
+
+    /**
+     * Method that loads plugins migrate task
+     * @return void
+     */
+    public function migrate()
+    {
+        $this->Migrate->main();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOptionParser()
+    {
+        $parser = parent::getOptionParser();
+        $parser
+            ->addSubcommand('migrate', ['help' => 'Migrate all loaded plugins']);
+
+        return $parser;
+    }
+}
