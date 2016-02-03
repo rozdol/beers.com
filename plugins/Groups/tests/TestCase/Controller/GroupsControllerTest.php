8f1910ebd864fc557c123d5aa64af0fd76898b31
George Constantinou <g.constantinou@qobo.biz>
2016-02-03 16:43:13 +0200

    baked Groups plugin Groups controller (task #805)

---
+++ plugins/Groups/tests/TestCase/Controller/GroupsControllerTest.php
@@ -0,0 +1,71 @@
+<?php
+namespace Groups\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestCase;
+use Groups\Controller\GroupsController;
+
+/**
+ * Groups\Controller\GroupsController Test Case
+ */
+class GroupsControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.groups.groups'
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     */
+    public function testView()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     */
+    public function testAdd()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     */
+    public function testEdit()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
