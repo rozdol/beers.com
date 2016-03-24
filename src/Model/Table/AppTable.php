7afac2edf3242d8acfc005cf11d0dcd13f8bf2cc
George Constantinou <g.constantinou@qobo.biz>
2016-03-24 14:52:00 +0200

    add common AppTable class and activate AuditStash's AuditLog behavior for all inherited tables (task #1397)

---
+++ src/Model/Table/AppTable.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+/**
+ * App Model
+ */
+class AppTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->addBehavior('AuditStash.AuditLog');
+    }
+}
