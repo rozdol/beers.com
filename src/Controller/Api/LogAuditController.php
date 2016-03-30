5b4347caa4dc413fce964bdadff5e33040e55ff2
George Constantinou <g.constantinou@qobo.biz>
2016-03-30 11:00:16 +0300

    api changelog results functionality (task #1397)

--- src/Controller/Api/LogAuditController.php
+++ src/Controller/Api/LogAuditController.php
@@ -5,4 +5,21 @@ use App\Controller\Api\AppController;
 
 class LogAuditController extends AppController
 {
+    /**
+     * Return log audit results for specific record.
+     *
+     * @return void
+     */
+    public function changelog()
+    {
+        $result = [];
+        if (isset($this->request->query['query'])) {
+            $id = $this->request->query['query'];
+            $query = $this->LogAudit->findByPrimaryKey($id)->order(['LogAudit.timestamp' => 'DESC']);
+            $result = $query->toArray();
+        }
+
+        $this->set('changelog', $result);
+        $this->set('_serialize', ['changelog']);
+    }
 }
