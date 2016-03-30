4c2ccaa5c78f7b7329136fb7c77310ea8d3e8aeb
George Constantinou <g.constantinou@qobo.biz>
2016-03-30 10:55:43 +0300

    move api CORS and lookup functionality from crm project (task #1397)

--- src/Controller/Api/AppController.php
+++ src/Controller/Api/AppController.php
@@ -16,7 +16,8 @@ class AppController extends Controller
                 'Crud.View',
                 'Crud.Add',
                 'Crud.Edit',
-                'Crud.Delete'
+                'Crud.Delete',
+                'Crud.Lookup'
             ],
             'listeners' => [
                 'Crud.Api',
@@ -31,4 +32,44 @@ class AppController extends Controller
         'limit' => 10,
         'maxLimit' => 100,
     ];
+
+    /**
+     * Lookup CRUD action events handling logic.
+     * @return \Cake\Network\Response
+     */
+    public function lookup()
+    {
+        $this->Crud->on('beforeLookup', function (\Cake\Event\Event $event) {
+            if (!empty($this->request->query['query'])) {
+                $displayField = $this->{$this->name}->displayField();
+                $primaryKey = $this->{$this->name}->primaryKey();
+                $this->paginate['conditions'] = [$displayField . ' LIKE' => '%' . $this->request->query['query'] . '%'];
+            }
+        });
+
+        return $this->Crud->execute();
+    }
+
+    /**
+     * Before filter handler.
+     * @param  \Cake\Event\Event $event The event.
+     * @return mixed
+     * @link   http://book.cakephp.org/3.0/en/controllers/request-response.html#setting-cross-origin-request-headers-cors
+     */
+    public function beforeFilter(Event $event)
+    {
+        parent::beforeFilter($event);
+
+        $this->response->cors($this->request)
+            ->allowOrigin(['*'])
+            ->allowMethods(['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
+            ->allowHeaders(['X-CSRF-Token', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept'])
+            ->maxAge(300)
+            ->build();
+
+        // if request method is OPTIONS just return the response with appropriate headers.
+        if ('OPTIONS' === $this->request->method()) {
+            return $this->response;
+        }
+    }
 }
