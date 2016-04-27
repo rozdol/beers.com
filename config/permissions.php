13dc6cb1c14b0be6cdb2bc0021cb46ed7a4ec202
Antonis Flangofas <a.flangofas@qobo.biz>
2016-04-27 13:17:34 +0300

    Allow from users plugin (task #1882)

---
+++ config/permissions.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Users.SimpleRbac.permissions' => [
+        [
+            'role' => '*',
+            'plugin' => '*',
+            'controller' => '*',
+            'action' => '*',
+        ],
+    ]];
