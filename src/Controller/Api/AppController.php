176922a058f37c270b459a728b41922fb2cd042c
George Constantinou <g.constantinou@qobo.biz>
2016-09-21 11:44:39 +0300

    add API Documentation basic info (task #2246)

--- src/Controller/Api/AppController.php
+++ src/Controller/Api/AppController.php
@@ -3,6 +3,16 @@ namespace App\Controller\Api;
 
 use CsvMigrations\Controller\Api\AppController as BaseController;
 
+/**
+    @SWG\Swagger(
+        @SWG\Info(
+            title="PROJECT_NAME API Documentation",
+            description="The PROJECT_NAME API allows interaction with PROJECT_NAME PROJECT_TYPE from your own application.",
+            termsOfService="http://swagger.io/terms/",
+            version="1.0.0"
+        )
+    )
+*/
 class AppController extends BaseController
 {
 }
