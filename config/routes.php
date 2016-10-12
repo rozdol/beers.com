007884856d721b3f76089480ef74ad80cd9d75d2
George Constantinou <g.constantinou@qobo.biz>
2016-10-12 18:28:27 +0300

    set homepage to Search plugin's Dashboards (task #2702)

--- config/routes.php
+++ config/routes.php
@@ -59,7 +59,7 @@ Router::scope('/', function ($routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    $routes->connect('/', ['plugin' => 'Search', 'controller' => 'Dashboards', 'action' => 'index']);
 
     $routes->connect('/users/', ['plugin' => 'CakeDC/Users', 'controller' => 'Users', 'action' => 'index']);
     $routes->connect('/users/:action/*', ['plugin' => 'CakeDC/Users', 'controller' => 'Users']);
