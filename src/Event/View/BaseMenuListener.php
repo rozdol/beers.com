f68fe9b06c1f646c0268f9207dbbfc331acb6630
George Constantinou <g.constantinou@qobo.biz>
2017-01-16 10:40:29 +0200

    modified Menu Listeners to support new menu rendering logic (task #3151)

---
+++ src/Event/View/BaseMenuListener.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Event\View;
+
+use Cake\Event\Event;
+use Cake\Event\EventListenerInterface;
+use Cake\Network\Exception\ForbiddenException;
+use Cake\ORM\TableRegistry;
+use Cake\Utility\Hash;
+
+abstract class BaseMenuListener implements EventListenerInterface
+{
+    /**
+     * Method that does acl check on flat (single level) menu items.
+     *
+     * @param  \Cake\Event\Event $event Event object
+     * @param  array             $menu  Menu
+     * @param  array             $user  User
+     * @return void
+     */
+    public function beforeRenderFlatMenu(Event $event, array $menu, array $user)
+    {
+        if (empty($menu)) {
+            return;
+        }
+
+        // if empty user try to get it from the SESSION
+        if (empty($user) && isset($_SESSION)) {
+            $user = Hash::get($_SESSION, 'Auth.user');
+            // if user still empty add all menu items to Event result and return
+            if (empty($user)) {
+                foreach ($menu as $item) {
+                    $event->result .= $item['html'] . ' ';
+                }
+
+                return;
+            }
+        }
+
+        $table = TableRegistry::get('RolesCapabilities.Capabilities');
+        foreach ($menu as $item) {
+            // this is for label like menu items without a url
+            if (empty($item['url'])) {
+                $event->result .= $item['html'] . ' ';
+                continue;
+            }
+
+            try {
+                $table->checkAccess($item['url'], $user);
+                $event->result .= $item['html'] . ' ';
+            } catch (ForbiddenException $e) {
+                // do nothing
+            }
+        }
+    }
+}
--- src/Event/View/ViewMenuListener.php
+++ src/Event/View/ViewMenuListener.php
@@ -2,11 +2,10 @@
 namespace App\Event\View;
 
 use Cake\Event\Event;
-use Cake\Event\EventListenerInterface;
 use Cake\Network\Request;
 use Cake\ORM\Entity;
 
-class ViewMenuListener implements EventListenerInterface
+class ViewMenuListener extends BaseMenuListener
 {
     /**
      * Menu element name
@@ -21,50 +20,144 @@ class ViewMenuListener implements EventListenerInterface
     public function implementedEvents()
     {
         return [
-            'View.View.Menu.Top' => 'getViewMenuTop'
+            'Search.Dashboards.View.View.Menu.Top' => 'getDashboardsViewMenuTop',
+            'Search.View.View.Menu.Actions' => 'getSearchResultsIndexMenuActions',
+            'CsvMigrations.Associated.actionsMenu.beforeRender' => 'beforeRenderFlatMenu',
+            'CsvMigrations.View.topMenu.beforeRender' => 'beforeRenderCsvMigrationsViewTopMenu',
+            'CsvMigrations.Dblists.Index.topMenu.beforeRender' => 'beforeRenderFlatMenu',
+            'CsvMigrations.Dblists.Index.actionsMenu.beforeRender' => 'beforeRenderFlatMenu',
+            'CsvMigrations.DblistItems.Index.topMenu.beforeRender' => 'beforeRenderFlatMenu',
+            'CsvMigrations.DblistItems.Index.actionsMenu.beforeRender' => 'beforeRenderFlatMenu',
         ];
+    }
+    /**
+     * Method that adds elements to CsvMigrations view View top menu.
+     *
+     * @param  \Cake\Event\Event $event Event object
+     * @param  array             $menu  Menu
+     * @param  array             $user  User
+     * @return void
+     */
+    public function beforeRenderCsvMigrationsViewTopMenu(Event $event, array $menu, array $user)
+    {
+        $url = [
+            'plugin' => $event->subject()->plugin,
+            'controller' => $event->subject()->name,
+            'action' => 'changelog',
+            $event->subject()->passedArgs[0]
+        ];
+
+        $html = $event->subject()->Html->link(
+            '<i class="fa fa-book"></i>',
+            $url,
+            ['title' => __('Changelog'), 'escape' => false]
+        );
+
+        array_unshift($menu, [
+            'html' => $html,
+            'url' => $url
+        ]);
+
+        $this->beforeRenderFlatMenu($event, $menu, $user);
     }
 
     /**
-     * Method that adds elements to view View top menu.
+     * Method that adds elements to Dashboards view View top menu.
      *
-     * @param  \Cake\Event\Event     $event   Event object
-     * @param  \Cake\Network\Request $request Request object
-     * @param  array                 $options Entity options
-     * @return string
+     * @param  Cake\Event\Event     $event   Event object
+     * @param  Cake\Network\Request $request Request object
+     * @param  Cake\ORM\Entity      $entity  Entity object
+     * @return void
      */
-    public function getViewMenuTop(Event $event, Request $request, array $options)
+    public function getDashboardsViewMenuTop(Event $event, Request $request, Entity $entity)
     {
-        $menu = [];
-        $html = null;
-
-        $urlChangelog = [
+        $urlEdit = [
             'plugin' => $request->plugin,
             'controller' => $request->controller,
-            'action' => 'changelog',
-            $options['entity']->id
+            'action' => 'edit',
+            $entity->id
         ];
-
-        $btnChangelog = $event->subject()->Html->link(
-            '',
-            $urlChangelog,
-            ['title' => __('Changelog'), 'class' => 'btn btn-default glyphicon glyphicon-book']
+        $btnEdit = ' ' . $event->subject()->Html->link(
+            '<i class="fa fa-pencil"></i>',
+            $urlEdit,
+            ['escape' => false, 'title' => __('Edit')]
         );
 
-        $menu[] = [
-            'label' => $btnChangelog,
-            'url' => $urlChangelog,
-            'capabilities' => 'fromUrl'
+        $urlDel = [
+            'plugin' => $request->plugin,
+            'controller' => $request->controller,
+            'action' => 'delete',
+            $entity->id
+        ];
+        $btnDel = ' ' . $event->subject()->Form->postLink(
+            '<i class="fa fa-trash"></i>',
+            $urlDel,
+            [
+                'confirm' => __('Are you sure you want to delete {0}?', $entity->name),
+                'title' => __('Delete'),
+                'escape' => false
+            ]
+        );
+
+        $menu = [
+            [
+                'label' => $btnEdit,
+                'url' => $urlEdit,
+                'capabilities' => 'fromUrl'
+            ],
+            [
+                'label' => $btnDel,
+                'url' => $urlDel,
+                'capabilities' => 'fromUrl'
+            ]
         ];
 
         if ($event->subject()->elementExists(static::MENU_ELEMENT)) {
-            $html = $event->subject()->element(static::MENU_ELEMENT, ['menu' => $menu, 'renderAs' => 'provided']);
+            $event->result .= $event->subject()->element(static::MENU_ELEMENT, ['menu' => $menu, 'renderAs' => 'provided']);
         } else {
-            $html .= $btnChangelog;
+            $event->result .= $btnEdit . $btnDel;
+        }
+    }
+
+    /**
+     * Method that adds elements to Search results index View actions menu.
+     *
+     * @param  \Cake\Event\Event      $event  Event object
+     * @param  \Cake\ORM\Entity|array $entity Entity
+     * @param  string                 $model  Model name
+     * @return void
+     */
+    public function getSearchResultsIndexMenuActions(Event $event, $entity, $model)
+    {
+        if ($entity instanceof Entity) {
+            $entity = $entity->toArray();
         }
 
-        $event->result = $html . $event->result;
+        list($plugin, $controller) = pluginSplit($model);
 
-        return $event->result;
+        $btnView = $event->subject()->Html->link(
+            '<i class="fa fa-eye"></i>',
+            ['plugin' => $plugin, 'controller' => $controller, 'action' => 'view', $entity['id']],
+            ['title' => __('View'), 'class' => 'btn btn-default btn-sm', 'escape' => false]
+        );
+
+        $menu = [
+            [
+                'label' => $btnView,
+                'url' => [
+                    'plugin' => $plugin,
+                    'controller' => $controller,
+                    'action' => 'view',
+                    $entity['id']
+                ],
+                'capabilities' => 'fromUrl'
+            ]
+        ];
+
+        if ($event->subject()->elementExists(static::MENU_ELEMENT)) {
+            $event->result .= $event->subject()->element(static::MENU_ELEMENT, ['menu' => $menu, 'renderAs' => 'provided']);
+        } else {
+            $event->result .= $btnView;
+        }
     }
 }
