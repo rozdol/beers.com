152258373fc99c242da1cc89c4dbad877a416e9f
George Constantinou <g.constantinou@qobo.biz>
2017-01-18 17:40:17 +0200

    added mini logo functionality (task #3151)

--- config/admin_lte.php
+++ config/admin_lte.php
@@ -5,20 +5,31 @@
 
 // get logo path
 $path = WWW_ROOT . 'img' . DS . 'logo.png';
-
 // convert to base64 image
 $data = file_get_contents($path);
 $base64 = 'data:image/png;base64,' . base64_encode($data);
-
 // create logo html img
 $logo = '<img src="' . $base64 . '" alt="Site Logo" />';
+
+// get mini logo path
+$path = WWW_ROOT . 'img' . DS . 'logo-mini.png';
+if (file_exists($path)) {
+    // convert to base64 image
+    $data = file_get_contents($path);
+    $base64 = 'data:image/png;base64,' . base64_encode($data);
+    // create mini logo html img
+    $logoMini = '<img src="' . $base64 . '" alt="Site Logo" />';
+} else {
+    $logoMini = $logo;
+}
+
 
 return [
     'Theme' => [
         'folder' => ROOT,
         'title' => getenv('PROJECT_NAME'),
         'logo' => [
-            'mini' => $logo,
+            'mini' => $logoMini,
             'large' => $logo,
         ],
         'login' => [
