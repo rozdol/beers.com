515403516adad346286ee7e6afc3ec3a64e9b829
Michael Stepanov <michael@stepanoff.org>
2017-05-04 10:26:34 +0300

    Changed default menu item icon as per comment

--- config/menu.php
+++ config/menu.php
@@ -5,6 +5,6 @@ return [
         // call getMenu method only once
         'allControllers' => false,
         // Default icon for menu item
-        'default_menu_item_icon' => 'bars',
+        'default_menu_item_icon' => 'cube',
     ],
 ];
