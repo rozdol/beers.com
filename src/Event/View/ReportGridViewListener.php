ffa46b3ecfc3673347e5fe9127c678157a60123d
Michael Stepanov <michael@stepanoff.org>
2017-05-18 17:03:03 +0300

    Added report grid event listener

---
+++ src/Event/View/ReportGridViewListener.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Event\View;
+
+use Cake\Event\Event;
+use Cake\Event\EventListenerInterface;
+use Cake\Log\LogTrait;
+use Cake\Network\Exception\ForbiddenException;
+use CsvMigrations\FieldHandlers\FieldHandlerFactory;
+
+class ReportGridViewListener implements EventListenerInterface
+{
+    use LogTrait;
+
+    /**
+     * @return array of implemented events for sets module
+     */
+    public function implementedEvents()
+    {
+        return [
+            'Search.Dashboard.Widget.GridElement' => 'processFieldValue',
+        ];
+    }
+
+    /**
+     *  processFieldValue method
+     *
+     *  Returns back processed field's value as per its DB type
+     *
+     * @param Cake\Event\Event $event of the current request
+     * @param string $model model name
+     * @param string $field field name
+     * @param string $value field value
+     * @return string processed field value
+     */
+    public function processFieldValue(Event $event, $model, $field, $value, $options = [])
+    {
+        $result = '';
+
+        $fhf = new FieldHandlerFactory();
+        $result = $fhf->renderValue($model, $field, $value);
+
+        $this->log(__METHOD__ . ": model=$model; field=$field; value=$value; result=$result", 'info');
+
+        return $result;
+    }
+}
