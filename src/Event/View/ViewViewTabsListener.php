c840bf2a8ee8a13fb6e22531ab0269db18179474
George Constantinou <georgec20001@gmail.com>
2017-06-14 15:36:17 +0300

    Filter out related tabs based on current user permissions (task #3996)

---
+++ src/Event/View/ViewViewTabsListener.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Event\View;
+
+use Cake\Event\Event;
+use Cake\Event\EventListenerInterface;
+use Cake\Http\ServerRequest;
+use Cake\ORM\Entity;
+use RolesCapabilities\CapabilityTrait;
+
+class ViewViewTabsListener implements EventListenerInterface
+{
+    use CapabilityTrait;
+
+    /**
+     * Implemented Events
+     * @return array
+     */
+    public function implementedEvents()
+    {
+        return [
+            'CsvMigrations.View.View.TabsList' => [
+                'callable' => 'getTabsList',
+                'priority' => 20
+            ]
+        ];
+    }
+
+    /**
+     * getTabsList method.
+     *
+     * Return the list of associations for the Entity as the tabs.
+     *
+     * @param \Cake\Event $event passed
+     * @param \Cake\Http\ServerRequest $request from the view
+     * @param \Cake\ORM\Entity $entity passed
+     * @param array $user User info
+     * @param array $options Extra options
+     * @return void
+     */
+    public function getTabsList(Event $event, ServerRequest $request, Entity $entity, array $user, array $options)
+    {
+        if (empty($event->result['tabs'])) {
+            return;
+        }
+
+        foreach ($event->result['tabs'] as $key => $value) {
+            list($plugin, $controller) = pluginSplit($value['targetClass']);
+            $url = ['plugin' => $plugin, 'controller' => $controller, 'action' => 'index'];
+            if ($this->_checkAccess($url, $user)) {
+                continue;
+            }
+
+            // remove tabs that user has no permission to access
+            unset($event->result['tabs'][$key]);
+        }
+    }
+}
