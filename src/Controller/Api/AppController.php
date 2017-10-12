803b4e8ff86e9ec231a9f7b7eae88688f694d0d5
George Constantinou <georgec20001@gmail.com>
2017-10-12 09:49:03 +0300

    Added API access check on APP controller (task #4603)

--- src/Controller/Api/AppController.php
+++ src/Controller/Api/AppController.php
@@ -1,8 +1,25 @@
 <?php
 namespace App\Controller\Api;
 
+use Cake\Network\Exception\ForbiddenException;
 use CsvMigrations\Controller\Api\AppController as BaseController;
+use RolesCapabilities\CapabilityTrait;
 
 class AppController extends BaseController
 {
+    use CapabilityTrait;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function initialize()
+    {
+        parent::initialize();
+
+        $hasAccess = $this->_checkAccess($this->request->params, $this->Auth->user());
+
+        if (!$hasAccess) {
+            throw new ForbiddenException();
+        }
+    }
 }
