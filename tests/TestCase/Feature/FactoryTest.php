edd3c98243519e7250698b221b3109a7da24bfdd
George Constantinou <georgec20001@gmail.com>
2017-11-17 16:40:04 +0200

    Drop tests for removed parameters (task #4839)

--- tests/TestCase/Feature/FactoryTest.php
+++ tests/TestCase/Feature/FactoryTest.php
@@ -4,38 +4,19 @@ namespace App\Test\TestCase\Feature;
 use App\Feature\Factory;
 use App\Feature\FeatureInterface;
 use App\Feature\Type\BaseFeature;
-use Cake\Controller\Component\AuthComponent;
-use Cake\Http\ServerRequest;
 use Cake\TestSuite\TestCase;
-use RuntimeException;
 
 /**
  * App\Feature\Factory Test Case
  */
 class FactoryTest extends TestCase
 {
-    public function setUp()
-    {
-        parent::setUp();
-
-        $this->auth = $this->createMock(AuthComponent::class);
-        $this->request = $this->createMock(ServerRequest::class);
-    }
-
-    public function tearDown()
-    {
-        unset($this->auth);
-        unset($this->request);
-
-        parent::tearDown();
-    }
-
     /**
      * For now we just make sure the Factory can be initialized, no assertions are made.
      */
     public function testInit()
     {
-        Factory::init($this->auth, $this->request);
+        Factory::init();
     }
 
     public function testGet()
