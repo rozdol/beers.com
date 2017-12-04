09d84b9fa91aeede71a1263c45d639dfee063989
Andrey Vystavkin <jujav4ik@gmail.com>
2017-12-04 14:39:02 +0200

    Modifying ExceptionRenderer for Logged users (task #4982)
    We don't want to break user navigation for loggedin users.
    In case we get 4xx/5xx error, we replace the content of the
    page but keepting sidebars and nav-bar components.
    
    It works only for disabled debug. Otherwise, the page
    will be using native CakePHP error templates.

---
+++ src/Error/AppExceptionRenderer.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Error;
+
+use Cake\Core\Configure;
+use Cake\Error\ExceptionRenderer;
+
+class AppExceptionRenderer extends ExceptionRenderer
+{
+    /**
+     * Render Error Page.
+     *
+     * We overwrite the render function just to remain
+     * on the same layout in case the user got an error
+     * and still logged in to the sytem.
+     *
+     * @return mixed
+     */
+    public function render()
+    {
+        $isDebug = Configure::read('debug');
+        $this->controller->loadComponent('CakeDC/Users.UsersAuth');
+        $this->controller->Auth->config('authorize', false);
+        $currentUser = $this->controller->Auth->user();
+
+        if (!$isDebug) {
+            if (!empty($currentUser)) {
+                $exception = $this->error;
+                $code = $this->_code($exception);
+                $message = $this->_message($this->error, $this->error->getCode());
+
+                $data = [
+                    'code' => $code,
+                    'message' => $message,
+                    'url' => $this->controller->request->getRequestTarget(),
+                    'error' => $this->_unwrap($exception),
+                    '_serialize' => ['message', 'code', 'url'],
+                ];
+
+                $res = $this->controller->request->session()->write('currentError', json_encode($data));
+
+                return $this->controller->redirect('/system/error');
+            }
+        }
+
+        return parent::render();
+    }
+}
