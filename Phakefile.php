444b9066431a08ff38b1aad11bc0ce9a78bba542
George Constantinou <georgec20001@gmail.com>
2017-12-07 17:54:45 +0200

    Re-added accidentally deleted line (task #5039)

--- Phakefile.php
+++ Phakefile.php
@@ -229,6 +229,7 @@ group('cakephp', function () {
             printInfo("Testing migration for plugin $plugin");
             $command = getenv('CAKE_CONSOLE') . " migrations migrate --quiet -p $plugin --connection=test";
             printInfo("Command: $command");
+            doShellCommand($command);
         }
         // Run app migrations
         printInfo("Testing application migrations");
