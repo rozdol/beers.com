7bf78cf1e40948b72023396eec592b99ea08f4e1
Andrey Vystavkin <jujav4ik@gmail.com>
2017-12-07 13:07:02 +0200

    Adding External API testing for users actions (task #5024)

--- tests/TestCase/Controller/Api/UsersControllerTest.php
+++ tests/TestCase/Controller/Api/UsersControllerTest.php
@@ -5,8 +5,10 @@ use App\Event\Controller\Api\EditActionListener;
 use App\Event\Controller\Api\IndexActionListener;
 use App\Event\Controller\Api\ViewActionListener;
 use Cake\Core\Configure;
+use Cake\Datasource\ConnectionManager;
 use Cake\Event\EventList;
 use Cake\Event\EventManager;
+use Cake\Http\Client;
 use Cake\Network\Exception\UnauthorizedException;
 use Cake\ORM\TableRegistry;
 use Cake\TestSuite\IntegrationTestCase;
@@ -22,6 +24,13 @@ class UsersControllerTest extends IntegrationTestCase
         'plugin.CakeDC/Users.users',
     ];
 
+    /**
+     * External API Client object
+     *
+     * @var \Cake\Http\Client for external api calls.
+     */
+    protected $apiClient = null;
+
     public function setUp()
     {
         parent::setUp();
@@ -30,6 +39,13 @@ class UsersControllerTest extends IntegrationTestCase
 
         // set headers without auth token by default.
         $this->setHeaders();
+
+        $this->apiClient = new Client([
+            'host' => 'localhost:8000',
+            'scheme' => 'http',
+        ], [
+            'type' => 'json',
+        ]);
 
         EventManager::instance()->on(new EditActionListener());
         EventManager::instance()->on(new IndexActionListener());
@@ -166,5 +182,96 @@ class UsersControllerTest extends IntegrationTestCase
         $entity = $this->Users->get($id);
 
         $this->assertEquals($data['first_name'], $entity->get('first_name'));
+    }
+
+    public function testExternalApiUsersCRUD()
+    {
+        $response = $this->apiClient->post('/api/users/token.json', [
+            'username' => getenv('DEV_USER'),
+            'password' => getenv('DEV_USER'),
+        ]);
+
+        $this->assertTrue($response->isOk(), "Couldn't fetch API token from users");
+
+        if (!$response->isOk()) {
+            return;
+        }
+
+        $responseBody = json_decode($response->body(), true);
+        unset($response);
+
+        $token = $responseBody['data']['token'];
+        $response = $this->sendAuthPost('/api/users/index.json', [], ['token' => $token]);
+        $this->assertTrue($response->isOk());
+
+        if (!$response->isOk()) {
+            return;
+        }
+
+        $usersResponse = json_decode($response->body(), true);
+
+        $this->assertTrue($usersResponse['success'], "Failed response on API users index call");
+        $this->assertNotEmpty($usersResponse['data'], "No users found in API /users/index.json");
+        unset($response);
+
+        $data = [
+            'username' => 'test-api-external',
+            'password' => 'test-api-external',
+            'active' => 1,
+            'is_superuser' => 0,
+        ];
+
+        $response = $this->sendAuthPost('/api/users/add.json', $data, ['token' => $token]);
+        $this->assertTrue($response->isOk());
+
+        if (!$response->isOk()) {
+            return;
+        }
+
+        $userCreated = json_decode($response->body(), true);
+        unset($response);
+
+        $response = $this->sendAuthPost(
+            '/api/users/view/' . $userCreated['data']['id'] . '.json',
+            [],
+            ['token' => $token]
+        );
+        $this->assertTrue($response->isOk());
+
+        $userView = json_decode($response->body(), true);
+
+        $this->assertEquals($userCreated['data']['id'], $userView['data']['id']);
+        unset($response);
+
+        $response = $this->sendAuthPost(
+            '/api/users/edit/' . $userView['data']['id'] . '.json',
+            ['is_superuser' => 1],
+            ['token' => $token]
+        );
+
+        $this->assertTrue($response->isOk());
+        unset($response);
+
+        $response = $this->sendAuthPost('/api/users/delete/' . $userView['data']['id'] . '.json', [], ['token' => $token]);
+
+        $this->assertTrue($response->isOk());
+        unset($response);
+
+        $response = $this->sendAuthPost('/api/users/view/' . $userView['data']['id'] . '.json', [], ['token' => $token]);
+        $this->assertFalse($response->isOk());
+    }
+
+    protected function sendAuthPost($url = '', $data = [], $headers = [])
+    {
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $headers['token'],
+            ],
+            'type' => 'json',
+        ];
+
+        $response = $this->apiClient->post($url, $data, $options);
+
+        return $response;
     }
 }
