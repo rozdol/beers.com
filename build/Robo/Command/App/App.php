9bd63683648ad418a4fc847ad148cad6f66dd8ce
Leonid Mamchenkov <leonid@mamchenkov.net>
2017-12-15 09:29:33 +0200

    Cleanup WordPress tasks (task #4292)

--- build/Robo/Command/App/App.php
+++ build/Robo/Command/App/App.php
@@ -11,7 +11,6 @@ class App extends AbstractCommand
      * @var array $defaultEnv Default values if missing in env
      */
     protected $defaultEnv = [
-        'SYSTEM_COMMAND_WPCLI'  => './vendor/bin/wp --allow-root --path=webroot/wp'
     ];
 
     /**
@@ -29,13 +28,7 @@ class App extends AbstractCommand
             return false;
         }
 
-        if (array_key_exists('PLATFORM', $env) && preg_match('/^qobrix.*$/', $env['PLATFORM'])) {
-            $result = $this->installCake($env);
-        } else if (array_key_exists('PLATFORM', $env) && preg_match('/^wordpress.*$/', $env['PLATFORM'])) {
-            $result = $this->installWp($env);
-        } else {
-            $result = true;
-        }
+        $result = $this->installCake($env);
 
         if (!$result) {
             return false;
@@ -59,13 +52,7 @@ class App extends AbstractCommand
             return false;
         }
 
-        if (preg_match('/^qobrix.*$/', $env['PLATFORM'])) {
-            $result = $this->installCake($env);
-        } else if (preg_match('/^wordpress.*$/', $env['PLATFORM'])) {
-            $result = $this->installWp($env);
-        } else {
-            $result = true;
-        }
+        $result = $this->installCake($env);
 
         if (!$result) {
             return false;
@@ -109,154 +96,6 @@ class App extends AbstractCommand
 
         // Remove .env
         return (file_exists('.env') && !unlink('.env')) ? false : true;
-    }
-
-    /**
-     * Do wordpress related install things
-     *
-     * @param array $env Environment variables
-     * @return bool true on success or false on failure
-     */
-    protected function installWp($env)
-    {
-        // Check DB connectivity and get server time
-        $result = $this->taskMysqlBaseQuery()
-            ->query("SELECT NOW() AS ServerTime")
-            ->user($this->getValue('DB_ADMIN_USER', $env))
-            ->pass($this->getValue('DB_ADMIN_PASS', $env))
-            ->host($this->getValue('DB_HOST', $env))
-            ->run();
-
-        if (!$result->wasSuccessful()) {
-            return false;
-        }
-        $this->say(implode(": ", $result->getData()['data'][0]['output']));
-
-        // prepare all remaining tasks in this array
-        $tasks = [];
-
-        // create DB
-        $tasks []= $this->taskMysqlDbCreate()
-            ->db($this->getValue('DB_NAME', $env))
-            ->user($this->getValue('DB_ADMIN_USER', $env))
-            ->pass($this->getValue('DB_ADMIN_PASS', $env))
-            ->host($this->getValue('DB_HOST', $env));
-
-        // Parse install script template
-        $tasks []= $this->taskTemplateProcess()
-            ->wrap('%%')
-            ->tokens($env)
-            ->src('etc/wp-cli.install')
-            ->dst('etc/wp-cli.install.sh');
-
-        // Run install script
-        $tasks []= $this->taskExec('/bin/bash etc/wp-cli.install.sh');
-
-        // Parse content script template
-        $tasks []= $this->taskTemplateProcess()
-            ->wrap('%%')
-            ->tokens($env)
-            ->src('etc/wp-cli.content')
-            ->dst('etc/wp-cli.content.sh');
-
-        // Run content script
-		$tasks []= $this->taskExec('/bin/bash etc/wp-cli.content.sh');
-
-        // Chmod dir
-		$tasks []= $this->taskFileChmod()
-			->path([$this->getValue('CHMOD_PATH', $env)])
-		    ->fileMode(0664)
-			->dirMode(0775)
-			->recursive(true);
-
-        // Chown dir
-		$tasks []= $this->taskFileChown()
-			->path([$this->getValue('CHOWN_PATH', $env)])
-			->user($this->getValue('CHOWN_USER', $env))
-			->recursive(true);
-
-        // Chgrp dir
-		$tasks []= $this->taskFileChgrp()
-			->path([$this->getValue('CHGRP_PATH', $env)])
-			->group($this->getValue('CHGRP_GROUP', $env))
-			->recursive(true);
-
-        // Now as we have all tasks prepared in order,
-        // run one-by-one and stop on first fail
-        foreach ($tasks as $task) {
-            $result = $task->run();
-            if (!$result->wasSuccessful()) {
-                return false;
-            }
-        }
-
-        // shoul be ok by here
-        return true;
-    }
-
-
-    /**
-     * Update a wordpress project
-     *
-     * @param array $env Environment variables
-     * @return bool true on success or false on failure
-     */
-    public function updateWp($env)
-    {
-        $result = $this->taskMysqlBaseQuery()
-            ->query("SELECT NOW() AS ServerTime")
-            ->user($this->getValue('DB_ADMIN_USER', $env))
-            ->pass($this->getValue('DB_ADMIN_PASS', $env))
-            ->host($this->getValue('DB_HOST', $env))
-            ->run();
-
-        if (!$result->wasSuccessful()) {
-            return false;
-        }
-        $this->say(implode(": ", $result->getData()['data'][0]['output']));
-
-        $tasks = [];
-
-        $tasks []= $this->taskTemplateProcess()
-            ->wrap('%%')
-            ->tokens($env)
-            ->src('etc/wp-cli.update')
-            ->dst('etc/wp-cli.update.sh');
-
-        $tasks []= $this->taskMysqlDbFindReplace()
-            ->search($this->getValue('DB_FIND', $env))
-            ->replace($this->getValue('DB_REPLACE', $env))
-            ->db($this->getValue('DB_NAME', $env))
-            ->user($this->getValue('DB_ADMIN_USER', $env))
-            ->pass($this->getValue('DB_ADMIN_PASS', $env))
-            ->host($this->getValue('DB_HOST', $env));
-
-		$tasks []= $this->taskExec('/bin/bash etc/wp-cli.update.sh');
-
-		$tasks []= $this->taskFileChmod()
-			->path([$this->getValue('CHMOD_PATH', $env)])
-			->fileMode(0664)
-			->dirMode(0775)
-			->recursive(true);
-
-		$tasks []= $this->taskFileChown()
-			->path([$this->getValue('CHOWN_PATH', $env)])
-			->user($this->getValue('CHOWN_USER', $env))
-			->recursive(true);
-
-		$tasks []= $this->taskFileChgrp()
-			->path([$this->getValue('CHGRP_PATH', $env)])
-			->group($this->getValue('CHGRP_GROUP', $env))
-			->recursive(true);
-
-        foreach ($tasks as $task) {
-            $result = $task->run();
-            if (!$result->wasSuccessful()) {
-                return false;
-            }
-        }
-
-        return true;
     }
 
     /**
