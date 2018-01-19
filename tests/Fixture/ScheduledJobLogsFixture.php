98ff400d5765b1417766bd1bc3f5b031cdda68cc
Andrey Vystavkin <jujav4ik@gmail.com>
2018-01-19 15:32:12 +0200

    Fixing fixtures to fix the fixes (task #5165)

--- tests/Fixture/ScheduledJobLogsFixture.php
+++ tests/Fixture/ScheduledJobLogsFixture.php
@@ -40,11 +40,11 @@ class ScheduledJobLogsFixture extends TestFixture
      */
     public $records = [
         [
-            'id' => 'f162ffc7-6b69-4133-913b-609170837615',
-            'scheduled_job_id' => '0a7dcbcb-1cf7-4258-8e1c-2db769d9f7ef',
-            'context' => 'Lorem ipsum dolor sit amet',
-            'status' => 'Lorem ipsum dolor sit amet',
-            'datetime' => '2018-01-18 14:33:33',
+            'id' => '00000000-0000-0000-0000-000000000001',
+            'scheduled_job_id' => '00000000-0000-0000-0000-000000000001',
+            'context' => '{"foo":"bar"}',
+            'status' => true,
+            'datetime' => '2018-01-18 09:00:00',
             'created' => '2018-01-18 14:33:33'
         ],
     ];
