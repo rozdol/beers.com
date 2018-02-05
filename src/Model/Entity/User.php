4fa71bb59cd8bad3ee1ad673f6c6be778196b692
George Constantinou <georgec20001@gmail.com>
2018-02-05 15:30:21 +0200

    Removed code which is now handled by custom database type (task #5327)

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -31,27 +31,6 @@ class User extends BaseUser
     }
 
     /**
-     * User image accessor
-     *
-     * Converts image resource into a string.
-     *
-     * @param resource $image Image resource
-     * @return string
-     */
-    protected function _getImage($image)
-    {
-        if (Configure::read('Users.gravatar.active') && $this->get('email')) {
-            return $this->getGravatar($this->get('email'));
-        }
-
-        if (is_resource($image)) {
-            return stream_get_contents($image);
-        }
-
-        return $image;
-    }
-
-    /**
      * Gravatar getter.
      *
      * @param string $email User email
