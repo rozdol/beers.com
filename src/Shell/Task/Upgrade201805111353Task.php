59457c7c5ccbd43db0d9204b515ee418016d0dd5
Andrey Vystavkin <jujav4ik@gmail.com>
2018-05-11 17:41:26 +0300

    Adding Default Scheduled Jobs for system wide ops (task #5165)
    We add by default certain shell commands, to avoid forgetting
    to patch crontabs if any.

---
+++ src/Shell/Task/Upgrade201805111353Task.php
@@ -0,0 +1,102 @@
+<?php
+namespace App\Shell\Task;
+
+use App\Feature\Factory as FeatureFactory;
+use App\ScheduledJobs\Handlers\CakeShellHandler;
+use Cake\Console\ConsoleOptionParser;
+use Cake\Console\Shell;
+use Cake\I18n\Time;
+use Cake\ORM\TableRegistry;
+
+class Upgrade201805111353Task extends Shell
+{
+    public $commandsToAdd = [
+        'CakeShell::App:database_log' => [
+            // every 5 hours
+            'recurrence' => 'FREQ=HOURLY;INTERVAL=5',
+        ],
+        'CakeShell::CsvMigrations:import' => [
+            // every 5 minutes
+            'recurrence' => 'FREQ=MINUTELY;INTERVAL=5'
+        ]
+    ];
+
+    /**
+     * Manage the available sub-commands along with their arguments and help
+     *
+     * @see http://book.cakephp.org/3.0/en/console-and-shells.html#configuring-options-and-generating-help
+     * @return \Cake\Console\ConsoleOptionParser
+     */
+    public function getOptionParser()
+    {
+        $parser = new ConsoleOptionParser('console');
+        $parser->setDescription('Adding default scheduled jobs to db, if not added before.');
+
+        return $parser;
+    }
+
+    /**
+     * main() method
+     *
+     * @retun void
+     */
+    public function main()
+    {
+        $feature = FeatureFactory::get('Module' . DS . 'ScheduledJobs');
+        if (! $feature->isActive()) {
+            $this->out('Scheduled Jobs are disabled. Skipping...');
+            return;
+        }
+
+        $cakeShellsHandler = new CakeShellHandler();
+
+        $list = $cakeShellsHandler->getList();
+
+        foreach ($list as $command) {
+            if (in_array($command, array_keys($this->commandsToAdd))) {
+                $this->addScheduleJob($command);
+            }
+        }
+    }
+
+    protected function addScheduleJob($command = '')
+    {
+        $result = false;
+
+        if (empty($command)) {
+            return $result;
+        }
+
+        $scheduledJobsTable = TableRegistry::get('ScheduledJobs');
+
+        $query = $scheduledJobsTable->find()
+            ->where(['job' => $command]);
+
+        $query->execute();
+
+        if ($query->count()) {
+            $entity = $query->first();
+            $this->out("Scheduled Job [$command] already added. Status [$entity->active]");
+
+            return $result;
+        }
+
+        $entity = $scheduledJobsTable->newEntity();
+        $entity->job = $command;
+        $entity->recurrence = $this->commandsToAdd[$command]['recurrence'];
+        $entity->active = true;
+        $entity->start_date = Time::now();
+
+        $saved = $scheduledJobsTable->save($entity);
+
+        if ($saved) {
+            $this->out("Added Scheduled Job [$command] to the datatable.");
+            $result = true;
+        } else {
+            $this->warning("Error adding scheduled job [$command] to database");
+            $result = false;
+        }
+
+        return $result;
+    }
+}
